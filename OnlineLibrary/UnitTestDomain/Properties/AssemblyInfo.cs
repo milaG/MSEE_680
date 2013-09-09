09931e44daf22b83333b6d7702bc001179943342
milaG <milatrifonov@yahoo.com>
2013-09-08 22:47:25 -0600

    This is the final Week 2 deliverable

--- OnlineLibrary/UnitTestDomain/Properties/AssemblyInfo.cs
+++ OnlineLibrary/UnitTestDomain/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DomainTest")]
+[assembly: AssemblyTitle("UnitTestDomain")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DomainTest")]
+[assembly: AssemblyProduct("UnitTestDomain")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c31c92aa-f389-4570-ac27-5f8ca2e263fe")]
+[assembly: Guid("2444b352-1e55-452c-a451-fddf0192e2c3")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ OnlineLibrary/UnitTestDomain/UnitTest1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnlineLibrary;
+using System.Linq; 
+
+namespace UnitTestDomain
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAddBook()
+        {
+            LibraryEntities Db = new LibraryEntities();
+
+            Book bb = new Book();
+            bb.BookTitle = "the hobbit";
+            bb.Author = "tolkien";
+            bb.BookDownload = "yes";
+
+            Db.Books.Add(bb);
+            Db.SaveChanges();
+        }
+
+        [TestMethod]
+        public void TestAddDvd()
+        {
+            LibraryEntities Db = new LibraryEntities();
+
+            DVD dvd = new DVD();
+            dvd.DvdTitle = "Superman";
+            dvd.DvdRating = "PG";
+
+            Db.DVDs.Add(dvd);
+            Db.SaveChanges();
+        }
+
+        [TestMethod]
+        public void TestAddMember()
+        {
+            LibraryEntities Db = new LibraryEntities();
+
+            Member memb = new Member();
+            memb.FirstName = "Susan";
+            memb.LastName = "Smith";
+            memb.Username = "userrtest";
+            memb.Password = "123";
+
+            Db.Members.Add(memb);
+            Db.SaveChanges();
+        }
+
+        [TestMethod]
+        public void TestDeleteBook()
+        {
+            LibraryEntities Db = new LibraryEntities();
+
+            Book deleteBook = (from del in Db.Books where del.BookTitle == "the hobbit" select del).FirstOrDefault();
+            Db.Books.Remove(deleteBook);
+            Db.SaveChanges();
+        }
+
+        [TestMethod]
+        public void TestDeleteDVD()
+        {
+            LibraryEntities Db = new LibraryEntities();
+
+            DVD deleteDVD = (from del in Db.DVDs where del.DvdTitle == "Superman" select del).FirstOrDefault();
+            Db.DVDs.Remove(deleteDVD);
+            Db.SaveChanges();
+        }
+
+        [TestMethod]
+        public void TestDeleteMember()
+        {
+            LibraryEntities Db = new LibraryEntities();
+
+            Member deleteMember = (from del in Db.Members where del.Username == "userrtest" select del).FirstOrDefault();
+            Db.Members.Remove(deleteMember);
+            Db.SaveChanges();
+        }
+
+    }
+}
